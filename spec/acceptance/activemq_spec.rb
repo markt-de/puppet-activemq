61554c468f57251a55ca479f5e08c4b58d3f8fcd
Michiel Hendriks <michiel.hendriks@nipv.nl>
2023-11-14 11:53:32 +0100

    Extended acceptance test with a static cluster config
    This will verify if a cluster config for a live-only cluster
    is valid.

--- spec/acceptance/activemq_spec.rb
+++ spec/acceptance/activemq_spec.rb
@@ -1,5 +1,7 @@
 require 'spec_helper_acceptance'
 
+RSpec::Matchers.define_negated_matcher :not_match, :match
+
 describe 'activemq' do
   let(:activemq_checksum) { 'a73331cb959bb0ba9667414682c279bc9ce2aec4c8fecbcdee4670bf9d63bf66010c8c55a6b727b1ad6d51bbccadd663b96a70b867721d9388d54a9391c6af85' }
   let(:activemq_group) { 'activemq' }
@@ -41,13 +43,28 @@ describe 'activemq' do
         admin_password => '#{activemq_instance_password}',
         admin_user => '#{activemq_instance_user}',
         checksum => '#{activemq_checksum}',
-        cluster => false,
+        cluster => true,
+        cluster_password => '#{activemq_instance_password}',
+        cluster_user => '#{activemq_instance_user}',
+        cluster_name => 'activemq-cluster',
+        cluster_topology => {
+          '#{activemq_instance_name}' => {
+            target_host => 'localhost',
+            bind => '127.0.0.1',
+          },
+          'instance2' => {
+            target_host => 'localhost',
+            bind => '127.0.0.2',
+          }
+        },
+        server_discovery => 'static',
         download_url => 'http://archive.apache.org/dist/activemq/activemq-artemis/#{activemq_version}/%s',
         instances => {
           '#{activemq_instance_name}' => {
             bind => '127.0.0.1',
             port => 61616,
             web_port => 8161,
+            ha_policy => 'live-only',
             acceptors => {
               artemis => { port => 61616 },
               amqp => { port => 5672 },
@@ -71,6 +88,14 @@ describe 'activemq' do
     it { expect(file("#{activemq_instance_base}/#{activemq_instance_name}/etc/artemis.profile").content).to match(%r{^HAWTIO_ROLE}) }
     it { expect(file("#{activemq_instance_base}/#{activemq_instance_name}/etc/artemis.profile").content).to match(%r{^ARTEMIS_HOME}) }
     it { expect(file("#{activemq_instance_base}/#{activemq_instance_name}/etc/artemis.profile").content).to match(%r{^JAVA_ARGS}) }
+
+    # Test that 'artemis-instance1' is not in the static-connectors list
+    it {
+      expect(file("#{activemq_instance_base}/#{activemq_instance_name}/etc/broker.xml").content.gsub(%r{.*<static-connectors[^>]*>(.*)</static-connectors>.*}ms, '\1'))
+        .to match(%r{instance2})
+        .and not_match(%r{:activemq_instance_name})
+    }
+    it { expect(file("#{activemq_instance_base}/#{activemq_instance_name}/etc/broker.xml").content).to match(%r{allow-direct-connections-only="true"}) }
 
     it 'sets up the service' do
       expect(service("activemq@#{activemq_instance_name}")).to be_running
